0c37ae888dc1c462497a91c8488bbc3955d6ecd7
Nguyễn Minh Hiếu <hieunguyen250507@gmail.com>
2026-03-31 07:39:08 +0700

    Initial project template

---
+++ src/main/java/com/example/hethongdaugia/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.hethongdaugia;
+
+import javafx.application.Application;
+
+public class Launcher {
+    static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
