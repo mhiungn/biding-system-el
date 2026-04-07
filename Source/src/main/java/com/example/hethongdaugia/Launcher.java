89c713502b70366be87316d29cc45cf458b27caf
Nguyễn Minh Hiếu <hieunguyen250507@gmail.com>
2026-04-07 09:31:19 +0700

    final framework

--- Source/src/main/java/com/example/hethongdaugia/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.hethongdaugia;
-
-import javafx.application.Application;
-
-public class Launcher {
-    static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
